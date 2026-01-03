cb2719de61ded2c885f805ad395dc1825fa37acf
Admin <taoufikfrihat@gmail.com>
2026-01-03 11:02:40 +0100

    hosting v0 of the project demo version

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
+    path: 'product/:id', // Matches your product details route
+    renderMode: RenderMode.Client // <--- CRITICAL FIX: Tells Angular "Don't build this now, do it in the browser"
+  },
+  {
+    path: '**', // All other routes (home, contact, etc.)
+    renderMode: RenderMode.Prerender // These will still be built as static HTML
   }
 ];
